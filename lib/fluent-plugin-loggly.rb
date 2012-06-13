26943ff34d3006dc5d2c9b2e9f2847b1dd9bd1a4
Patrik Antonsson <patant@gmail.com>
2012-06-13 22:29:18 +0200

    Revert "removed files from gem generation"
    This reverts commit e087a0c66754a3482af96d78b81461ae328d20c1.

---
+++ lib/fluent-plugin-loggly.rb
@@ -0,0 +1,9 @@
+require "fluent-plugin-loggly/version"
+
+module Fluent
+  module Plugin
+    module Loggly
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/fluent-plugin-loggly/version.rb
@@ -0,0 +1,7 @@
+module Fluent
+  module Plugin
+    module Loggly
+      VERSION = "0.0.1"
+    end
+  end
+end
